07461e6f92a0b99b03ea3cbb0b7845e278c6ef27
Carolina Ruiz <ing.acrd@gmail.com>
2023-11-13 14:52:12 -0500

    just modify hello world to test

--- Assignment3/src/main/java/ca/college/lasalle/Main.java
+++ Assignment3/src/main/java/ca/college/lasalle/Main.java
@@ -2,6 +2,6 @@ package ca.college.lasalle;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world! Carolina edited");
     }
 }
