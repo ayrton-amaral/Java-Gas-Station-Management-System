2ac8e18bbc443cc85a6040daf7ddb444c1626b78
Carolina Ruiz <ing.acrd@gmail.com>
2023-11-15 10:55:09 -0500

    created menu class and messages fixed on gas class

--- Assignment3/src/main/java/ca/college/lasalle/Gas.java
+++ Assignment3/src/main/java/ca/college/lasalle/Gas.java
@@ -9,11 +9,11 @@ public class Gas extends Product{
 
     public void sell(double amount){
         if(amount == 0) {
-            throw new RuntimeException("The tanks are empty.");
+            throw new RuntimeException("Invalid amount required, has to be more than 0");
         }
         if(amountInTheTanks < amount){
             throw new RuntimeException(String
-                    .format("Not enough fuel int the tanks. (%s L available)", this.amountInTheTanks));
+                    .format("Not enough fuel in the tanks. (%s L available)", this.amountInTheTanks));
         }
         this.amountInTheTanks -= amount;
     }
