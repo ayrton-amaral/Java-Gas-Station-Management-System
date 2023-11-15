44b300445c931b7d94791673151c61ced28451df
Ayrton Amaral <150078902+ayrton-amaral@users.noreply.github.com>
2023-11-15 14:23:30 -0500

    Update Main.java

--- Assignment3/src/main/java/ca/college/lasalle/Main.java
+++ Assignment3/src/main/java/ca/college/lasalle/Main.java
@@ -2,6 +2,8 @@ package ca.college.lasalle;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world! Carolina edited");
+        System.out.println("Test2");
     }
 }
